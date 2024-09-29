d868fc19aa6bb4676afd9b65e22fd54237dd6692
Felipe Velozo <felipe_velozo@hotmail.com>
2024-09-29 12:30:35 -0300

    Meu novo commit, em uma nova branch

---
+++ main.py
@@ -0,0 +1,10 @@
+nome = "Felipe Velozo"
+
+idade = 31
+
+Objetivo = "estudar Engenharia de Dados"
+
+print(f"Meu nome é: {nome}, tenho {idade} anos e meu objetivo é: {Objetivo}")
+
+
+print("Fazendo uma modificação nova, para aparecer na nova branch")
