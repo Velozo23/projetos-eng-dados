295b9e43bee6786a601ba025fda56814490d7425
Felipe Velozo <felipe_velozo@hotmail.com>
2024-09-29 12:48:48 -0300

    Validação de branch

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ print(f"Meu nome é: {nome}, tenho {idade} anos e meu objetivo é: {Objetivo}")
 
 
 print("Fazendo uma modificação nova, para aparecer na nova branch")
+
+print("Realizando um novo teste")
+
+print("Nova validação")
+
+print("Novamente testando a branch jkhhjhj")
