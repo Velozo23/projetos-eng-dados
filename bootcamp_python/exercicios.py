2e0e13320050317a2b4073ba49df3261bf56d193
Felipe Velozo <felipe_velozo@hotmail.com>
2024-10-13 15:35:01 -0300

    Adionando outro código ao projeto

---
+++ bootcamp_python/exercicios.py
@@ -0,0 +1,11 @@
+# Escreva um programa que soma 2 numeros inteiros inseridos pelo usuário.
+
+# Recebe um numero digitado pelo usuário.
+n1 = int(input("Digite um numero: "))
+
+# Recebe outro numero digitado pelo usuário.
+n2 = int(input("Digite outro numero: "))
+#Realiza o calculo da operação
+resultado = n1 // n2 
+print(f"O resultado do calculo é: {resultado} ")
+
